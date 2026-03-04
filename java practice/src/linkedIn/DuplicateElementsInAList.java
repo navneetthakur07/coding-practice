92b084ce16d53d5f68830afa796eb4d43b0fa127
Navneet Thakur <navneetthakurz47@gmail.com>
2026-03-04 09:29:38 +0530

    TA12422: many new classes have been added

--- java practice/src/linkedIn/DuplicateElementsInAList.java
+++ java practice/src/linkedIn/DuplicateElementsInAList.java
@@ -5,23 +5,45 @@ import java.util.*;
 public class DuplicateElementsInAList {
 
     public static void main(String[] args) {
-        List<String> words = Arrays.asList("Sanjay", "Raghav","Komal", "Komal", "Raghav");
-//        List<String> words = List.of("Sanjay", "Raghav", "Komal", "Raghav");
-//        Map<String, Integer> map = new HashMap<>();
-//
-//// we will use the key value pair for the List provided above according to count we will print the duplicate value
-////  As soon as we encounter the count we will print it
-//
-//        for(String word : words) {
-//            map.put(word, map.getOrDefault(word, 0)+1);
-//        }
-//
-//        for (Map.Entry<String, Integer> entry: map.entrySet())
-//        {
-//            if(entry.getValue()>1) {
-//                System.out.println(entry.getKey());
-//            }
-//        }
+        List<String> list = Arrays.asList("Sanjay", "Raghav","Komal", "Komal", "Raghav");
+        List<String> words = List.of("Sanjay", "Raghav", "Komal", "Raghav");
+        Map<String, Integer> map = new HashMap<>();
+
+        // we will use the key value pair for the List provided above according to count we will print the duplicate value
+        //  As soon as we encounter the count we will print it
+
+        for(String word : words) {
+            map.put(word, map.getOrDefault(word, 0)+1);
+        }
+
+        /*
+        When you call map.entrySet(), it gives you:
+        [ ("apple"=2), ("banana"=3), ("orange"=1) ]
+        A Set containing all these pairs as Entry objects.
+         */
+
+        for (Map.Entry<String, Integer> entry: map.entrySet())
+        {
+            if(entry.getValue()>1) {
+                System.out.println(entry.getKey());
+            }
+        }
+
+        // Alternative to the above approach
+
+        System.out.println("Using modern approach of for each");
+        map.forEach((word, count) -> {             // Gets both key and value directly
+            if (count > 1) {
+                System.out.println(word);
+            }
+        });
+
+        map.forEach((word,count) -> {
+            if(count>1) {
+                System.out.println(word);
+            }
+        }
+        );
 
         // now we will be going to use the stream for finding the duplicate value from a list of String
         // Here we have used the terminal operation which is given by the Consumer Functional Interface
--- java practice/src/linkedIn/SortedListOnStringLength.java
+++ java practice/src/linkedIn/SortedListOnStringLength.java
@@ -7,10 +7,24 @@ public class SortedListOnStringLength {
 
     public static void main(String[] args) {
 
-        List<String> names = List.of("Aman", "Sanjy", "Gauravandit", "Pankaj", "Anirudha Acharya");
+        List<String> names = List.of("Aman", "Ajay", "Sanjy", "Gauravandit", "Pankaj", "Anirudha Acharya");
 
+        // for printing the list in reverse order of length
         names.stream()
-                .sorted(Comparator.comparingInt(String::length).reversed())
+                .sorted(Comparator.comparingInt(String::length)
+                        .reversed())
+                .forEach(System.out::println);
+
+        // if we encounter the String with same length then it will sort on the basis of alphabetical order
+        // comparator.nauturalOrder() will work here it will sort on the basis of alphabetical order
+        /*
+        thenComparing()
+         */
+        names.stream()
+                .sorted(Comparator.comparingInt(String::length)
+                        .reversed()
+                        .thenComparing(Comparator.naturalOrder())
+                        .thenComparing(word-> word.startsWith("S")))
                 .forEach(System.out::println);
 
     }
